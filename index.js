f34b229810c63761ad56cd0d91a3b6632c85f2a8
nsk0826 <nsk.20160401@gmail.com>
2022-07-28 10:52:56 +0900

    add youCIty

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const cities = require("cities");
 const myCity = cities.zip_lookup("10016");
+const youCity = cities.zip_lookup("10017");
+
 console.log(myCity);
+
+console.log("youCity:" + youCity);
